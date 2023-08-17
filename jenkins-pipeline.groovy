d79dce0b211dfffd2f582ab8683f281647022f22
madhav2903 <141008318+madhav2903@users.noreply.github.com>
2023-08-17 16:01:41 +0530

    Update jenkins-pipeline.groovy

--- jenkins-pipeline.groovy
+++ jenkins-pipeline.groovy
@@ -8,7 +8,7 @@ pipeline {
         }
         stage('build'){
             steps {
-                sh 'ant'
+                sh 'ant -version'
             }
         }
         stage('executeSonarqubeReport')
